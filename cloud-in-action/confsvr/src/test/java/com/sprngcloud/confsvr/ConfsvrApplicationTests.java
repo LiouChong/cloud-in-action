700c265ec2a1cfb2b069d1a6c2390fe6b49de1a2
liuchong <970009721@qq.com>
2019-11-03 15:00:47 +0800

    feat: cloud-in-action 认证和配置模块

---
+++ cloud-in-action/confsvr/src/test/java/com/sprngcloud/confsvr/ConfsvrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprngcloud.confsvr;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfsvrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ cloud-in-action/licensing-service/src/test/java/com/cloud/licensingservice/LicensingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloud.licensingservice;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicensingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
